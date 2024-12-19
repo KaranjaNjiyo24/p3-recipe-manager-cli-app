3a55444b8f1a8c6a837dcd33fa8c9ff120e0480a
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-12-19 15:06:13 +0300

    Adds user input handling

--- recipe.py
+++ recipe.py
@@ -5,9 +5,20 @@ from models1 import Base, User, Recipe, Ingredient, RecipeIngredient
 
 DATABASE_URL = "sqlite:///recipes.db"
 engine = create_engine(DATABASE_URL)
-Session = sessionmaker(bind=engine)
+session = Session()
 
 def create_database():
     if not os.path.exists("recipes.db"):
         Base.metadata.create_all(engine)
         print("Database created successfully!")
+
+def get_user_input(prompt, required=True, input_type=str):
+    while True:
+        user_input = input(prompt)
+        if not user_input and required:
+            print("This field is required. Please provide a value.")
+            continue
+        try:
+            return input_type(user_input)
+        except ValueError:
+            print(f"Invalid input. Please enter a valid {input_type.__name__}.")
