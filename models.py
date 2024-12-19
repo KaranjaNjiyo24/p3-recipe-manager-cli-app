8c774ea28851400ec1867815791fc368fdf8060e
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-12-19 14:50:37 +0300

    Adds recipe model relationship to User and Ingredients

--- models.py
+++ models.py
@@ -11,3 +11,17 @@ class User(Base):
     username = Column(String, unique=True, nullable=False)
     email = Column(String, unique=True, nullable=True)
     recipes = relationship('Recipe', back_populates='user')
+
+# Recipe Model
+class Recipe(Base):
+    __tablename__ = 'recipes'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    name = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+    prep_time = Column(Integer, nullable=False)
+    cook_time = Column(Integer, nullable=False)
+    servings = Column(Integer, nullable=False)
+
+    user = relationship('User', back_populates='recipes')
+    ingredients = relationship('Ingredient', secondary='recipe_ingredients', back_populates='recipes')
