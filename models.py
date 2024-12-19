2a846ffa232f8fe2749ba7c1f0766768ae381cf3
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-12-19 14:49:22 +0300

    Adds user model with name and email field

--- models.py
+++ models.py
@@ -3,3 +3,11 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+# User Model
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=True)
+    recipes = relationship('Recipe', back_populates='user')
